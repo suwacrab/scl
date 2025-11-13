0a718f6df077f973c2e070b2c943e831a9ec5403
U-DOG\suwa <starcrabbs@gmail.com>
2025-11-12 22:15:34 -0800

    initial

---
+++ include/scl/container/blob.hpp
@@ -0,0 +1,117 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <cstdint>
+#include <cstdio>
+#include <bit>
+#include <exception>
+
+namespace scl {
+
+class blob {
+	public:
+		std::vector<uint8_t> m_data;
+
+		blob& write_blob(const blob& source) {
+			m_data.insert(m_data.end(),source.m_data.begin(),source.m_data.end());
+			return *this;
+		}
+		void write_raw(const void* source, size_t len) {
+			if(len == 0) return;
+			if(source == nullptr) {
+				std::puts("Blob::write_raw(): error: attempt to write from null source");
+				std::terminate();
+			}
+
+			auto src_bytes = static_cast<const uint8_t*>(source);
+			for(size_t i=0; i<len; i++) {
+				m_data.push_back(*src_bytes++);
+			}
+		}
+		constexpr auto write_u8(uint32_t n) -> void { m_data.push_back(n); }
+		constexpr auto write_u16(uint32_t n) -> void { write_u8(n); write_u8(n>>8); }
+		constexpr auto write_u32(uint32_t n) -> void { write_u16(n); write_u16(n>>16); }
+		constexpr auto write_u64(uint64_t n) -> void { write_u32(n); write_u32(n>>32); }
+		constexpr auto write_be_u16(uint16_t n) -> void { write_u16(std::byteswap(n)); }
+		constexpr auto write_be_u32(uint32_t n) -> void { write_u32(std::byteswap(n)); }
+		constexpr auto write_be_u64(uint64_t n) -> void { write_u64(std::byteswap(n)); }
+		void write_str(std::string_view str, bool no_terminator=false) {
+			for(size_t i=0; i<str.size(); i++) {
+				char chr = str.at(i);
+				write_u8(chr);
+			}
+			if(!no_terminator) {
+				write_u8(0);
+			}
+		}
+
+		void pad(int divisor, int padbyte = 0xAB) {
+			while((size()%divisor) != 0) {
+				write_u8(padbyte);
+			}
+		}
+		void clear() {
+			m_data.clear();
+		}
+		
+		bool file_load(const std::string& filename, bool strict=true) {
+			auto file = std::fopen(filename.c_str(),"rb");
+			if(!file) {
+				if(strict) {
+					std::printf("Blob::file_load(): error: unable to load file %s\n",
+						filename.c_str()
+					);
+					std::terminate();
+				}
+				return false;
+			}
+			// get file size ----------------------------@/
+			std::fseek(file,0,SEEK_END);
+			const size_t fsize = std::ftell(file);
+			std::rewind(file);
+
+			// write to blob ----------------------------@/
+			std::vector<uint8_t> buffer(fsize);
+			std::fread(buffer.data(),1,buffer.size(),file);
+			std::fclose(file);
+			m_data.insert(m_data.end(),buffer.begin(),buffer.end());
+			return true;
+		}
+		bool file_send(const std::string& filename, bool strict=true) {
+			auto file = std::fopen(filename.c_str(),"wb");
+			if(!file) {
+				if(strict) {
+					std::printf("Blob::file_send(): error: unable to send to file %s\n",
+						filename.c_str()
+					);
+					std::terminate();
+				}
+				return false;
+			}
+			std::fwrite(data(),sizeof(char),size(),file);
+			std::fclose(file);
+			return true;
+		}
+		blob compress(const bool do_compress=true);
+
+		template<typename T=void*> auto data() -> T {
+			return reinterpret_cast<T>(m_data.data());
+		}
+		constexpr auto size() const -> size_t { return m_data.size(); }
+		constexpr auto at(const size_t idx) -> uint8_t& { return m_data.at(idx); }
+
+		blob() {
+			clear();
+		}
+		blob(const blob& orig) {
+			m_data = orig.m_data;
+		}
+		blob(const std::vector<uint8_t>& data) {
+			m_data = data;
+		}
+};
+
+} // namespace scl
+
+
---
+++ source/tests/main.cpp
@@ -0,0 +1,84 @@
+#include <scl/container/blob.hpp>
+#include <ranges>
+#include <filesystem>
+
+namespace stdfs = std::filesystem;
+
+namespace sample_sdzarc {
+	typedef struct CFileEntry {
+
+	};
+
+	static void fn_pack() {
+		const std::string entry_names[] = {
+			"entry A.",
+			"entry B.",
+			"entry C!",
+			"lastly, entry D.",
+			"though, is even an entry E safe to use? not sure.",
+			"maybe even an entry F, if we feel like it.",
+			"also, the hidden prototype entry X."
+		};
+
+		std::vector<scl::blob> filedata_table;
+		std::vector<std::string> filename_table;
+		auto basepath = stdfs::path("../../cmdutil/miniarc");
+		std::printf("foldername: %s\n",basepath.string().c_str());
+		for (const auto & entry : stdfs::recursive_directory_iterator(basepath)) {
+			if(entry.is_regular_file()) {
+				auto path = entry.path().string();
+				auto proxim = stdfs::proximate(entry.path(),basepath);
+				scl::blob filedata;
+				std::printf("loading file arc::%s\n",proxim.c_str());
+				filedata.file_load(path);
+				filedata_table.push_back(filedata);
+				filename_table.push_back(proxim.string());
+			}
+		}
+
+		constexpr size_t pad_size = 256;
+		scl::blob blob_all;
+		scl::blob blob_segHeader;
+		scl::blob blob_segEntry;
+		scl::blob blob_segName;
+		scl::blob blob_segData;
+
+		std::puts("writing blobs");
+		for(auto const [index,data] : std::views::enumerate(filedata_table)) {
+			blob_segEntry.write_u32(0xE000);
+			blob_segEntry.write_u32(index);
+			blob_segEntry.write_u32(data.size());
+			blob_segEntry.write_u32(blob_segName.size());
+			blob_segName.write_str(filename_table.at(index));
+			blob_segData.write_blob(data);
+		}
+		std::puts("writing final");
+		blob_segEntry.pad(pad_size);
+		blob_segName.pad(pad_size);
+		blob_segData.pad(pad_size);
+
+		const std::size_t offset_segEntry = pad_size;
+		const std::size_t offset_segName = offset_segEntry + blob_segEntry.size();
+		const std::size_t offset_segData = offset_segName + blob_segName.size();
+		blob_segHeader.write_str("SDA");
+		blob_segHeader.write_u32(offset_segEntry);
+		blob_segHeader.write_u32(offset_segName);
+		blob_segHeader.write_u32(offset_segData);
+		blob_segHeader.pad(pad_size);
+
+		blob_all.write_blob(blob_segHeader)
+			.write_blob(blob_segEntry)
+			.write_blob(blob_segName)
+			.write_blob(blob_segData)
+			.file_send("out.bin",false);
+	}
+	static void unpack() {
+		
+	}
+};
+
+int main(int argc, const char* argv[]) {
+	sample_sdzarc::pack()
+	sample_sdzarc::unpack()
+}
+
